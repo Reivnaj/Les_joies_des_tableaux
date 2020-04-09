adf9053fab8088f34ae211a14e60c11476a4393a
Reivnaj <56923184+Reivnaj@users.noreply.github.com>
2020-04-09 20:06:51 +0200

    Ajout des exo
    Exo 00_journalists et 01_cryptocurrencies

---
+++ lib/00_journalists.rb
@@ -0,0 +1,133 @@
+def count(handle)
+  # Compte le nombre de journalistes dans le tableau
+  puts "Il y a #{handle.size} journalistes dans le tableau"
+end
+
+def has_digits?(str)
+  # On verifie si il y un chiffre
+  str.count("0-9") > 0
+end
+
+def contain_number(handle)
+  # Affiche le nombre de handle contenant un chiffre
+  sub_handle = []
+  handle.each do |str|
+    if has_digits?(str)
+      sub_handle << str
+    end
+  end
+  puts "Il y a #{sub_handle.size} contenant un chiffre"
+end
+
+def contain_Aude(handle)
+  #combien de handle contiennent la string "Aude"
+  sub_handle = []
+  handle.each do |str|
+    if str.match(/[Aa][Uu][Dd][Ee]/)
+      sub_handle << str
+    end
+  end
+  puts "Il y a #{sub_handle.size} handle qui contiennent Aude"
+end
+
+def start_uppercase(handle)
+  #combien de handle contiennent une majuscule en première lettre
+  sub_handle = []
+  handle.each do |str|
+    if str.match(/^@[A-Z]/)
+      sub_handle << str
+    end
+  end
+  puts "Il y a #{sub_handle.size} qui ont une majuscule en première lettre"
+end
+
+def contain_uppercase(handle)
+  #combien de handle contiennent au moins une majuscule
+  sub_handle = []
+  handle.each do |str|
+    #regex majuscule
+    if str.match(/[A-Z]/)
+      sub_handle << str
+    end
+  end
+  puts " #{sub_handle.size} handle contiennent au moins une majuscule"
+end
+
+def count_underscore(handle)
+#  sub_handle = []
+  sum = 0
+  handle.each do |str|
+    #regex underscore
+    if str.match(/_/)
+#      sub_handle << str
+#.scan(/_/) renvoie un tableau contenant chaque _ trouvé dans str.
+#.size renvoie la taille de ce tableau
+#donc le nombre de _ dans str
+      sum = sum + str.scan(/_/).size
+    end
+  end
+  puts "Il y a #{sum} underscores au total dans ce tableau"
+end
+
+def alphabetic_sort(handle)
+  handle.sort
+  return handle.sort
+end
+
+def fifty_shorter(handle)
+#on fait un hash vide
+  mon_hash = {}
+#on stock nos values(taille du handle) et keys (handle) dedans
+  handle.each do |str|
+    mon_hash[str] = str.size
+  end
+#on trie notre hash avec .sort_by
+  mon_hash = mon_hash.sort_by{|str, size| size}.to_h
+#on le retourne avec les 50 premières clefs
+  puts  "Les 50 handle les plus courts son : #{mon_hash.keys[0..49]}"
+end
+
+def position(handle)
+  #on fait un hash vide
+    mon_hash = {}
+    position = 0
+  #on stock nos values(taille du handle) et keys (handle) dedans
+    handle.each do |str|
+      mon_hash[str] = position
+      position +=1
+    end
+    puts "Le handle @epenser se trouve à la #{mon_hash["@epenser"]}-ème position (en comptant à partir de 0)"
+end
+
+def perform
+  # Initialisation du tableau de handle
+handle = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+  #Nombre de journalistes dans le tableau
+  count(handle)
+
+  #Nombre de handle contenant un chiffre
+  contain_number(handle)
+
+  #Nombre de handle contenant "Aude"
+  contain_Aude(handle)
+
+  #Nombre de handle commançant pas une majuscule
+  start_uppercase(handle)
+
+  #Nombre de handle contenant une majuscule
+  contain_uppercase(handle)
+
+  #Nombre total d'underscore
+  count_underscore(handle)
+
+  #Trié en ordre alphabetique
+  alphabetic_sort(handle)
+
+  #On affiche les 50 plus court
+  fifty_shorter(handle)
+
+  position(handle)
+end
+
+perform
